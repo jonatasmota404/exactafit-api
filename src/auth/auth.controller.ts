74baf31e09748b1a475d07151f8ff83482688191
Jônatas Júnior <jonatasjr.019@gmail.com>
2026-08-18 17:44:23 -0300

    feat(auth): implement login endpoint with jwt generation and bcrypt compare

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register.dto';
+import { LoginUserDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('register')
   create(@Body() dto: RegisterUserDto) {
     return this.authService.create(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginUserDto) {
+    return this.authService.login(dto)
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterUserDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService
+  ) {}
 
   async create(dataDto: RegisterUserDto) {
     const userExists = await this.prisma.user.findUnique({
@@ -29,5 +34,26 @@ export class AuthService {
 
     delete (user as { password?: string }).password;
     return user;
+  }
+
+  async login(dataDto: LoginUserDto){
+    const user = await this.prisma.user.findUnique({
+      where: { email: dataDto.email },
+    });
+
+    if (!user) {
+        throw new UnauthorizedException('Credenciais inválidas');
+    }
+    
+    const validPassword = await bcrypt.compare(dataDto.password, user.password)
+
+    if (!validPassword) {
+        throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    const payload = {sub: user.id, email: user.email };
+    const token = await this.jwtService.signAsync(payload);
+    return {accessToken: token };
+
   }
 }
